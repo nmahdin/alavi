058eed1bb98771975caaa4ab02673734a18cda4b
mahdi <mahdi.nabavy@gmail.com>
2025-03-15 05:58:17 +0330

    v 0.9.3

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,7 +43,7 @@ class AuthenticatedSessionController extends Controller
             $request->validate([
                 'name' => ['required', 'string', 'min:3', 'max:255'],
                 'number' => ['required', 'string', 'lowercase','min:11', 'max:11', 'unique:'.User::class],
-                'chair_number' => ['required', 'string', 'lowercase', 'max:11'],
+                'chair_number' => ['required', 'string', 'lowercase', 'max:11', 'unique:'.User::class],
                 'password' => ['required', 'confirmed', Rules\Password::defaults()],
             ], [
                 'number.required' => 'شماره تلفن الزامی است',
@@ -55,7 +55,8 @@ class AuthenticatedSessionController extends Controller
             if (Questions::count() == 0) {
                 $random = 0;
             } else {
-                $random = Questions::all()->pluck('id')->shuffle()->implode('.');
+//                $random = Questions::all()->pluck('id')->shuffle()->implode('.');
+                $random = "1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27.28.29.30.31.32.33.34.35.36.37.38.39.40.41.42.43.44.45.46.47.48.49.50.51.52.53.54.55.56.57.58.59.60.61.62.63.64.65.66.67.68.69.70.71.72.73.74.75.76.77.78.79.80";
             }
             $user = User::create([
                 'name' => $request->name,
